0452754fad6744a1ee1ba7a9da1068925312cd0b
JC <jasonqc9103@gmail.com>
2022-08-18 17:35:13 -0400

    add checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Authentication from './routes/authentication/authentication.components'
 import Home from './routes/home/home.components'
 import Navigation from './routes/navigation/navigation.components'
-
+import Checkout from './routes/checkout/checkout.components'
 import Shop from './routes/shop/shop.components'
 
 
@@ -16,7 +16,7 @@ const App = () => {
         <Route index element={<Home />} /> 
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
-
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   )
--- src/routes/shop/shop.components.jsx
+++ src/routes/shop/shop.components.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 
-import ProductCard from '../../components/product-card/prodcut-card.components'
+import ProductCard from '../../components/product-card/product-card.components'
 
-import { ProductContext } from '../../contexts/products.context'
+import { ProductsContext} from '../../contexts/products.context'
 
 import './shop.styles.scss'
 
 const Shop = () => {
-  const { products } = useContext(ProductContext)
+  const { products } = useContext(ProductsContext)
 
   return (
     <div className="products-container">
