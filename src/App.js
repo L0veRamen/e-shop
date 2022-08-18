ba41ab5278f89254a04698546a1a0a7e54c7fb70
JC <jasonqc9103@gmail.com>
2022-08-17 22:09:55 -0400

    add authentication components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 
-import SignIn from './routes/sign-in/sign-in.components'
+import Authentication from './routes/authentication/authentication.components'
 import Home from './routes/home/home.components'
 import Navigation from './routes/navigation/navigation.components'
 
@@ -17,7 +17,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} /> 
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
 
       </Route>
     </Routes>
